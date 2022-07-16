848f2992d2b525b049a28759fe4b142158561f52
mara-olson <mara.c.olson@gmail.com>
2022-07-16 15:29:45 -0700

    Appointment class added to model.py with relationships between users and appts created

--- model.py
+++ model.py
@@ -10,5 +10,22 @@ class User(db.Model):
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     username = db.Column(db.String, nullable=False, unique=True)
 
+    appt = db.relationship("Appointment", back_populates="user")
+
     def __repr__(self):
         return f'<User user_id={self.user_id} username={self.username}>'
+
+
+class Appointment(db.Model):
+    """Appointments for melon tasting booked by users."""
+
+    __tablename__ = "appointments"
+
+    appt_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    appt_time = db.Column(db.DateTime)
+
+    user = db.relationship("User", back_populates="appt")
+
+    def __repr__(self):
+        return f'<Appointment appt_id={self.appt_id} time={self.appt_time}>'
