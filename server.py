e393c7191c6c0c5154c2911bff6af02b31e66b67
mara-olson <mara.c.olson@gmail.com>
2022-07-23 12:21:53 -0500

    Removed debug mode from server

--- server.py
+++ server.py
@@ -126,4 +126,4 @@ def schedule_selected_appt():
 
 if __name__ == "__main__":
     connect_to_db(app)
-    app.run(host="0.0.0.0", port="5001", debug=True)
+    app.run(host="0.0.0.0", port="5001")
